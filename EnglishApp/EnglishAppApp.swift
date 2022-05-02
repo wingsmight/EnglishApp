907cfddbf8505a3b06ac92da6320acf6a89f0294
wingsmight <wingsmight@gmail.com>
2022-05-02 13:42:48 +0700

    Initial Commit

---
+++ EnglishApp/EnglishAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishAppApp.swift
+//  EnglishApp
+//
+//  Created by Igoryok on 02.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct EnglishAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
